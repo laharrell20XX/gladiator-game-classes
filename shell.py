38345b5bd317d6eecd0521d3b5e1d80a66e891e9
laharrell20XX <lharrell@basecampcodingacademy.org>
2018-07-19 16:25:00 -0500

    added a function to allow user to make attack decision

--- shell.py
+++ shell.py
@@ -13,11 +13,31 @@ def show_gladiators(gladiators):
         print(gladiator)
 
 
+def make_decision(attacker_name, gladiators):
+    while True:
+        decision = input('''{}... What would you like to do?
+- [a]ttack
+- [p]ass
+- [q]uit
+- [h]eal
+- [d]efend
+>>> '''.format(attacker_name)).lower()
+        if decision == 'a':
+            return decision
+        elif decision == 'h':
+            return decision
+        elif decision == 'q':
+            print('\n{}'.format('\n'.join(
+                gladiator.gladiator_name + ' survived!'
+                for gladiator in gladiators)))
+            quit()
+        else:
+            print('\nInvalid Input\n')
+
+
 def main():
     gladiator_1 = Gladiator(greeting('Player_1'), 100, 0, 5, 15)
     gladiator_2 = Gladiator(greeting('Player_2'), 100, 0, 5, 15)
-    gladiators = gladiator_1, gladiator_2
-    show_gladiators(gladiators)
 
 
 if __name__ == '__main__':

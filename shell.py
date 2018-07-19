678b0dc651c550920c5a055fe03477d9231cc05c
laharrell20XX <lharrell@basecampcodingacademy.org>
2018-07-19 15:07:30 -0500

    added a function to show gladiators

--- shell.py
+++ shell.py
@@ -6,9 +6,18 @@ def greeting(player):
     return gladiator_name
 
 
+def show_gladiators(gladiators):
+    ''' (gladiators) -> NoneType
+    '''
+    for gladiator in gladiators:
+        print(gladiator)
+
+
 def main():
     gladiator_1 = Gladiator(greeting('Player_1'), 100, 0, 5, 15)
     gladiator_2 = Gladiator(greeting('Player_2'), 100, 0, 5, 15)
+    gladiators = gladiator_1, gladiator_2
+    show_gladiators(gladiators)
 
 
 if __name__ == '__main__':
